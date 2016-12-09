8da323f3a80615da483857799992e0247f6b9d1a
Gareth Boden <gareth.boden@tapina.com>
2016-12-09 02:05:25 +0000

    Avoid caching RestTemplates to prevent tunnel close/open race condition at server end.

--- weblab-api-tunnel-server/src/main/java/ucles/weblab/common/tunnel/server/TunnelRestTemplateFactory.java
+++ weblab-api-tunnel-server/src/main/java/ucles/weblab/common/tunnel/server/TunnelRestTemplateFactory.java
@@ -1,10 +1,12 @@
 package ucles.weblab.common.tunnel.server;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 import java.util.Map;
@@ -16,10 +18,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * @since 25/11/2016
  */
 @RequiredArgsConstructor
+@Slf4j
 public class TunnelRestTemplateFactory {
     private final List<HttpMessageConverter<?>> messageConverterList;
     private final WebSocketResponseHandler webSocketResponseHandler;
-    private final Map<String, RestTemplate> restTemplates = new ConcurrentHashMap<>();
+    private final Map<String, WebSocketSession> webSocketSessions = new ConcurrentHashMap<>();
 
     private void configureMessageConverters(final RestTemplate restTemplate) {
         restTemplate.setMessageConverters(messageConverterList);
@@ -27,24 +30,35 @@ public class TunnelRestTemplateFactory {
 
     @EventListener(HttpTunnelUpEvent.class)
     public void handleTunnelUp(HttpTunnelUpEvent e) {
-        final WebSocketHttpRequestFactory requestFactory = new WebSocketHttpRequestFactory(e.getWebSocketSession(), webSocketResponseHandler);
-        final RestTemplate restTemplate = new RestTemplate(requestFactory);
-        configureMessageConverters(restTemplate);
-        restTemplates.put(e.getName(), restTemplate);
+        webSocketSessions.put(e.getName(), e.getWebSocketSession());
     }
 
     @EventListener(HttpTunnelDownEvent.class)
     public void handleTunnelDown(HttpTunnelDownEvent e) {
-        restTemplates.remove(e.getName());
+        if (!webSocketSessions.remove(e.getName(), e.getWebSocketSession())) {
+            log.debug("Ignoring tunnel down event as current session for {} does not match", e.getName());
+        }
     }
 
     public RestTemplate getRestTemplateForSource(String externalSourceName) {
-        final RestTemplate restTemplate;
-        restTemplate = restTemplates.get(externalSourceName);
+        final RestTemplate restTemplate = createRestTemplate(externalSourceName);
+
         if (restTemplate == null) {
             throw new ResourceAccessException("Web Socket tunnel not connected for " + externalSourceName);
         }
         return restTemplate;
+    }
+
+    private RestTemplate createRestTemplate(String name) {
+        final WebSocketSession webSocketSession = webSocketSessions.get(name);
+        if (webSocketSession == null) return null;
+        else {
+            log.debug("Create new RestTemplate for tunnel {}", name);
+            final WebSocketHttpRequestFactory requestFactory = new WebSocketHttpRequestFactory(webSocketSession, webSocketResponseHandler);
+            final RestTemplate restTemplate = new RestTemplate(requestFactory);
+            configureMessageConverters(restTemplate);
+            return restTemplate;
+        }
     }
 
 }
